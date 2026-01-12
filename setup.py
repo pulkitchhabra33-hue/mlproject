94e1bc1ca7356aeaddb4d3d1f2db8395492e1b67
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-01-12 08:12:02 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setup import find_packages, setup
+from typing import List
+
+hyphen_e_dot= '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements= [req.replace("\n", "") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+    
+    return requirements
+
+
+setup(
+    name= 'mlproject',
+    version= '0.0.1',
+    author= 'Pulkit Chhabra',
+    author_email= 'pulkitchhabra33@gmail.com',
+    packages= find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
