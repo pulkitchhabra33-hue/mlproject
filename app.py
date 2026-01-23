6d0fc80132c9e6202e274af3a68eb109dd505736
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-01-23 19:06:47 +0530

    End-to-end ML pipeline with data ingestion, transformation, training and MLflow tracking

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import sys
 
 if __name__== "__main__":
     logging.info("Application has started")
 
     try:
+        #data ingestion
         data_ingestion= DataIngestion()
         train_data_path, test_data_path= data_ingestion.initiate_data_ingestion()
 
+        #data transformation
         data_transformation= DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_array, test_array, _= data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        
+         ## Model Training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array, test_array))
 
     except Exception as e:
         logging.error("Application has failed", exc_info= True)
---
+++ src/mlproject/components/model_trainer.py
@@ -0,0 +1,150 @@
+import os
+import sys
+from dataclasses import dataclass
+from urllib.parse import urlparse
+from fastapi import params
+import mlflow
+import mlflow.sklearn
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+from xgboost import XGBRegressor
+
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+from src.mlproject.utils import save_object,evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path= os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config= ModelTrainerConfig()
+
+    def eval_metrics(self, actual, pred):
+        mae= mean_absolute_error(actual, pred)
+        mse= mean_squared_error(actual, pred)
+        rmse= np.sqrt(mse)
+        r2_square= r2_score(actual, pred)
+
+        return mae, rmse, r2_square
+    
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and testing input data")
+            X_train, y_train, X_test, y_test= (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models= {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
+            }
+            
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+
+            }
+
+            model_report: dict= evaluate_models(X_train, y_train, X_test, y_test, models, params)
+
+            ## To get best model score from dict
+            best_model_score= max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+            best_model_name= list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model= models[best_model_name]
+
+            print(f"So, this is the best model found: {best_model_name}")
+
+            # model_name= list(params.keys())
+            actual_model = best_model_name
+            best_params = params[actual_model]
+
+            mlflow.set_tracking_uri("https://dagshub.com/pulkitchhabra33-hue/mlproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+                rmse, mae, r2 = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                mlflow.sklearn.log_model(best_model, "model")
+
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best model found on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
